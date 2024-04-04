559db9b12dff0d4721417ee7217758a7d8e0cdd4
Casmir Onyeka <casmir293@gmail.com>
2024-04-04 21:17:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP TODO</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="task" placeholder="Input task...">
+    </form>
+</body>
+</html>
